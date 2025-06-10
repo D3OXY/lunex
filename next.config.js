b8480b289e1862f856fd2cb8d1da92ee2521f1b3
d3oxy <ishansachu1@gmail.com>
2025-06-10 22:41:37 +0530

    feat(config): add custom rewrites for routing in Next.js configuration

--- next.config.js
+++ next.config.js
@@ -5,6 +5,15 @@
 import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  rewrites: async () => {
+    return [
+      {
+        source: "/((?!api|_next/static|_next/image|favicon.ico|robots.txt).*)",
+        destination: "/shell",
+      },
+    ];
+  },
+};
 
 export default config;
