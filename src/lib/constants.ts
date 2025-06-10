5ab3771d20d82afc42e4bdab7497664ec858136a
d3oxy <ishansachu1@gmail.com>
2025-06-10 20:27:51 +0530

    feat: update application branding and environment configuration

---
+++ src/lib/constants.ts
@@ -0,0 +1,4 @@
+export const APP_NAME = "Lunex";
+export const APP_DESCRIPTION = "World's second best AI Chat App";
+export const APP_URL = "https://lunex.deoxy.dev";
+export const APP_VERSION = "0.0.1";
