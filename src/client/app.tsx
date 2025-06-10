ffb726585a31ad998ec9d9f0bfecd7964f7badbd
d3oxy <ishansachu1@gmail.com>
2025-06-10 20:41:18 +0530

    feat(router): add react-router and create Shell components for client side SPA routing

---
+++ src/client/app.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes } from "react-router";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      {/* <Layout> */}
+      <Routes>
+        {/* <Route path="/" element={<Home />} />
+                    <Route path="/docs" element={<Docs />} />
+                    <Route path="/examples" element={<Examples />} />
+                    <Route path="/items/:id" element={<ItemDetail />} />
+                    <Route path="*" element={<NotFound />} /> */}
+      </Routes>
+      {/* </Layout> */}
+    </BrowserRouter>
+  );
+}
