c27b0a309915edf1447affb39647e14e753d6e4e
d3oxy <ishansachu1@gmail.com>
2025-06-13 19:58:43 +0530

    feat(convex): add chats entity to schema for AI message handling

--- convex/schema.ts
+++ convex/schema.ts
@@ -2,15 +2,26 @@ import { defineEntSchema, defineEnt, getEntDefinitions } from "convex-ents";
 import { v } from "convex/values";
 
 const schema = defineEntSchema({
-  // Users
-  users: defineEnt({
-    email: v.string(),
-    name: v.string(),
-    imageUrl: v.optional(v.string()),
-  })
-    .field("clerkId", v.string(), { unique: true })
-    .field("username", v.string(), { unique: true })
-    .index("by_email", ["email"]),
+    // Users
+    users: defineEnt({
+        email: v.string(),
+        name: v.string(),
+        imageUrl: v.optional(v.string()),
+    })
+        .field("clerkId", v.string(), { unique: true })
+        .field("username", v.string(), { unique: true })
+        .index("by_email", ["email"]),
+    // AI
+    chats: defineEnt({
+        userId: v.id("users"),
+        title: v.string(),
+        messages: v.array(
+            v.object({
+                role: v.union(v.literal("user"), v.literal("assistant")),
+                content: v.string(),
+            })
+        ),
+    }),
 });
 
 export default schema;
