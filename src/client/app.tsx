65d669bbdaf9b8fa27d0ced90e1bff6c400e4e37
d3oxy <ishansachu1@gmail.com>
2025-06-14 00:12:21 +0530

    feat(app): add chat route to application for improved navigation

--- src/client/app.tsx
+++ src/client/app.tsx
@@ -1,19 +1,18 @@
 import NotFound from "@/app/not-found";
+import Chat from "@/client/chat";
 import Home from "@/client/home";
 import { BrowserRouter, Route, Routes } from "react-router";
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      {/* <Layout> */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        {/* <Route path="/docs" element={<Docs />} />
-                    <Route path="/examples" element={<Examples />} />
-                    <Route path="/items/:id" element={<ItemDetail />} /> */}
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-      {/* </Layout> */}
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            {/* <Layout> */}
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/chat/:id" element={<Chat />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+            {/* </Layout> */}
+        </BrowserRouter>
+    );
 }
