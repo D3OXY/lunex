132e88c249b21434649212c23433bb1c9c3a50c8
d3oxy <ishansachu1@gmail.com>
2025-06-16 13:56:53 +0530

    refactor(sidebar): enhance navbar to display dynamic chat titles based on current path

--- src/components/sidebar/navbar.tsx
+++ src/components/sidebar/navbar.tsx
@@ -1,12 +1,19 @@
+"use client";
+
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { SidebarTrigger } from "@/components/ui/sidebar";
+import { useChats, type Chat } from "@/lib/stores/chat-store";
 import { UserButton } from "@clerk/nextjs";
+import type { Id } from "convex/_generated/dataModel";
 import { Authenticated } from "convex/react";
 import { Bell } from "lucide-react";
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
+    const pathname = usePathname();
+    const chats = useChats();
     return (
         <nav className="bg-background flex h-10 shrink-0 items-center justify-between gap-2 rounded-lg px-4">
             <div className="flex h-10 items-center gap-2">
@@ -18,9 +25,16 @@ export default function Navbar() {
                             <BreadcrumbLink href="#">Lunex</BreadcrumbLink>
                         </BreadcrumbItem>
                         <BreadcrumbSeparator className="hidden md:block" />
-                        <BreadcrumbItem>
-                            <BreadcrumbPage>Dashboard</BreadcrumbPage>
-                        </BreadcrumbItem>
+                        {pathname === "/" && (
+                            <BreadcrumbItem>
+                                <BreadcrumbPage>New Chat</BreadcrumbPage>
+                            </BreadcrumbItem>
+                        )}
+                        {pathname.startsWith("/chat/") && (
+                            <BreadcrumbItem>
+                                <BreadcrumbPage className="max-w-[100px] truncate md:max-w-[300px]">{getChatTitle(chats, pathname.split("/")[2]! as Id<"chats">)}</BreadcrumbPage>
+                            </BreadcrumbItem>
+                        )}
                     </BreadcrumbList>
                 </Breadcrumb>
             </div>
@@ -34,4 +48,9 @@ export default function Navbar() {
             </Authenticated>
         </nav>
     );
+}
+
+function getChatTitle(chats: Chat[], chatId: Id<"chats">) {
+    const chat = chats.find((chat) => chat._id === chatId);
+    return chat?.title;
 }
